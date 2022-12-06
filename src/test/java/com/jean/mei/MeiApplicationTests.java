c33e7f40033e0f99e00df561c739197746ffcbd9
jcheavy <jcheavy@hotmail.com>
2022-12-06 15:31:25 -0300

    primeiro commit

---
+++ src/test/java/com/jean/mei/MeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jean.mei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
