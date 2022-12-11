4afd9b20e339366b5601f535798aafa1a528452d
jcheavy <jcheavy@hotmail.com>
2022-12-10 22:53:36 -0300

    Tratamento de erros

--- src/main/java/com/jean/mei/services/CategoriaService.java
+++ src/main/java/com/jean/mei/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jean.mei.entities.Categoria;
 import com.jean.mei.repositories.CategoriaRepository;
+import com.jean.mei.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,7 +18,9 @@ public class CategoriaService {
 	
 	public Categoria bucar(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		return obj.orElse(null);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 	public void salvar() {
