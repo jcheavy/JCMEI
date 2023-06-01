13fb06039f1a6d3d585a2cf96e0f30d2e9545b1d
jcheavy <jcheavy@hotmail.com>
2023-05-31 23:55:41 -0300

    Consulta cliente

--- src/main/java/com/jean/mei/entities/dto/ClienteDTO.java
+++ src/main/java/com/jean/mei/entities/dto/ClienteDTO.java
@@ -5,10 +5,12 @@ import java.io.Serializable;
 import org.hibernate.validator.constraints.Length;
 
 import com.jean.mei.entities.Cliente;
+import com.jean.mei.services.validation.ClienteUpdate;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
--- src/main/java/com/jean/mei/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/jean/mei/services/validation/ClienteUpdateValidator.java
@@ -2,43 +2,42 @@ package com.jean.mei.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.jean.mei.entities.Cliente;
-import com.jean.mei.entities.dto.ClienteNewDTO;
-import com.jean.mei.entities.enums.TipoCliente;
+import com.jean.mei.entities.dto.ClienteDTO;
 import com.jean.mei.repositories.ClienteRepository;
 import com.jean.mei.resources.exceptions.FieldMessage;
-import com.jean.mei.services.validation.utils.BR;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
 
+	@Autowired
+	private HttpServletRequest request;
 	@Autowired
 	private ClienteRepository repo;
 	
 	@Override
-	public void initialize(ClienteInsert ann) {
+	public void initialize(ClienteUpdate ann) {
 	}
 
 	@Override
-	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
 
-		List<FieldMessage> list = new ArrayList<>();
-
-		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
-		}
-
-		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
-		}
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
 		
+		Integer uriId = Integer.parseInt(map.get("id"));			
+		List<FieldMessage> list = new ArrayList<>();		
 		Cliente aux = repo.findByEmail(objDto.getEmail());
-		if (aux != null) {
+		
+		if (aux != null && !aux.getId().equals(uriId)) {
 			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
